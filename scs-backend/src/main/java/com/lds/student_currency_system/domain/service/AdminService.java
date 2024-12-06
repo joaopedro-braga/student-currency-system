67e4bec465e8f8462b91d96094815628ff4fb025
João Pedro Braga <joao.psbraga6@gmail.com>
2024-12-06 07:42:35 -0300

    refactor: simplify AdminService interface and implementation by removing unused methods

--- scs-backend/src/main/java/com/lds/student_currency_system/domain/service/AdminService.java
+++ scs-backend/src/main/java/com/lds/student_currency_system/domain/service/AdminService.java
@@ -1,20 +1,11 @@
 package com.lds.student_currency_system.domain.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import com.lds.student_currency_system.domain.model.Admin;
 
 public interface AdminService {
     Admin save(Admin admin);
-
-    Optional<Admin> findById(Long id);
-
+    
     Optional<Admin> findByEmail(String email);
-
-    List<Admin> findAll();
-
-    void deleteById(Long id);
-
-    Admin update(Admin admin);
 }
--- scs-backend/src/main/java/com/lds/student_currency_system/domain/service/impl/AdminServiceImpl.java
+++ scs-backend/src/main/java/com/lds/student_currency_system/domain/service/impl/AdminServiceImpl.java
@@ -5,10 +5,10 @@ import com.lds.student_currency_system.domain.model.Admin;
 import com.lds.student_currency_system.domain.service.AdminService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -22,30 +22,8 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public Optional<Admin> findById(Long id) {
-        return adminRepository.findById(id);
-    }
-
-    @Override
-    public List<Admin> findAll() {
-        return adminRepository.findAll();
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        adminRepository.deleteById(id);
-    }
-
-    @Override
-    public Admin update(Admin admin) {
-        if(adminRepository.existsById(admin.getId())) {
-            return adminRepository.save(admin);
-        }
-        throw new RuntimeException("Admin não encontrado.");
-    }
-
-    @Override
     public Optional<Admin> findByEmail(String email) {
         return adminRepository.findByEmail(email);
     }
+
 }
