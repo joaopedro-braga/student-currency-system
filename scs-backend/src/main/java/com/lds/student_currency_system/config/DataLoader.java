81d572a3aa2ce483da4f3ffe42fa52017574455a
João Pedro Braga <joao.psbraga6@gmail.com>
2024-12-06 15:31:34 -0300

    refactor: update DataLoader to use UserService and change admin email format

--- scs-backend/src/main/java/com/lds/student_currency_system/config/DataLoader.java
+++ scs-backend/src/main/java/com/lds/student_currency_system/config/DataLoader.java
@@ -1,15 +1,12 @@
 package com.lds.student_currency_system.config;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.lds.student_currency_system.domain.model.Admin;
-import com.lds.student_currency_system.domain.model.User;
 import com.lds.student_currency_system.domain.service.AdminService;
-import com.lds.student_currency_system.infra.security.TokenService;
+import com.lds.student_currency_system.domain.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,19 +15,14 @@ import lombok.RequiredArgsConstructor;
 public class DataLoader implements CommandLineRunner {
 
     private final AdminService adminService;
-
-    private final TokenService tokenService;
-    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
     @Override
     public void run(String... args) throws Exception {
 
-        if (adminService.findByEmail("admin@mail.com").isEmpty()) {
-            Admin admin = new Admin("Administrator", "admin@mail.com", new BCryptPasswordEncoder().encode("1234"));
+        if(!userService.existByEmail("admin@gmail.com")){
+            Admin admin = new Admin("Administrator", "admin@gmail.com", new BCryptPasswordEncoder().encode("123456"));
             adminService.save(admin);
         }
-
-        var usernamePassword = new UsernamePasswordAuthenticationToken("admin@mail.com", "1234");
-        var user = (User) this.authenticationManager.authenticate(usernamePassword).getPrincipal();
     }
 }
