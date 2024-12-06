676ede0e4e32638d488f9105ad4a639de6ff754e
João Pedro Braga <joao.psbraga6@gmail.com>
2024-12-06 17:34:10 -0300

    feat: add one-to-many relationship between Institution and Professor entities

--- scs-backend/src/main/java/com/lds/student_currency_system/domain/model/Institution.java
+++ scs-backend/src/main/java/com/lds/student_currency_system/domain/model/Institution.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +28,8 @@ public class Institution extends Company{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "institution", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Professor> professors;
     
 }
